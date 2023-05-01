3f8bbc214ffd068448e92e2bc33c6723e651180b
kattypo <74074509+kattypo@users.noreply.github.com>
2023-04-30 22:58:51 -0400

    Update Main.java
    Now supports a cycle input and assigning tasks to robots
    Assignment is best on best robot for the job among robots available with a max of 2 robots towards a task

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
 import javax.swing.*;
 import java.awt.*;
-import java.util.Scanner;
 
 //design specs:
 //robots - full commitment (finish task once started)
@@ -16,9 +13,57 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //taking user input is placeholder
+        //would like to ask for cycles through JPanel input
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the amount of cycles to display: ");
+        int cycles = scan.nextInt(); //amount of cycles to demo for
+        int cyclesCounter = 0;
+        boolean check = true;
+
         int[] initialPositions = generateInitialEnvironment();
         char[][] initialGrid = generateGrid(initialPositions);
         displayGrid(initialGrid);
+
+        while(cyclesCounter < cycles){ //will iterate for determined amount of cycles
+            for(int i = 0; i < robots.size(); i++){//robots should view list of tasks and pick one
+                if((robots.get(i).hasTask == false) && (tasks.size() > 0)){//verifies robot is not assigned task and there are tasks to assign
+                    for(int j = 0; j < tasks.size(); j++){
+                        if(tasks.get(j).assigned() < 2){
+                            int robotBid = robots.get(i).calculateBid(robots.get(i).row, robots.get(i).col, tasks.get(j).row, tasks.get(j).col);
+                            Bid bid = new Bid(robotBid, i);
+                            System.out.printf("Current Robot location y- %d x- %d and current task location y- %d x- %d\n", robots.get(i).row, robots.get(i).col, tasks.get(j).row, tasks.get(j).col);
+                            System.out.printf("Robot %d is placing a bid for task %d: %d\n", i, j, robotBid); //test
+                            tasks.get(j).addBid(bid);
+                        }
+                    }
+                }
+            }
+
+            for(int i = 0; i < tasks.size(); i++){//sorts the bids in each tasks
+                tasks.get(i).sortBids();
+            }
+
+            Collections.sort(tasks);//sorts the tasks by best preferred match for assignment
+
+            for(int place = 0; place < robots.size(); place++){ //will go through each task's preferred robot 1st,2nd, etc.
+                for(int i = 0; i < tasks.size(); i++){ //assigns robots their tasks
+                    if(robots.get(tasks.get(i).bidWinner(place)).hasTask == false && tasks.get(i).assigned() < 2){ //checks that bidwinner doesn't already have task
+                        robots.get(tasks.get(i).bidWinner(place)).assignTask(i);//assigns task to robot
+                        tasks.get(i).addRobot();//assigns robot to task
+                        System.out.printf("Task %d's has been assigned to: %d\n", i, tasks.get(i).bidWinner(place));
+                        System.out.printf("Task location: y-%d x-%d Robot location: y-%d x-%d\n", tasks.get(i).row, tasks.get(i).col, robots.get(tasks.get(i).bidWinner(0)).row, robots.get(tasks.get(i).bidWinner(0)).col);
+                    }
+                }
+            }
+            cyclesCounter += 1;
+            //prioritize each task having a robot
+            //if there are more robots than tasks, allow teamwork
+            //for each robot go through each available task (priortize 0 robot tasks first)
+            //for each robot go through each available task (not maxed out tasks next)
+            //generate bid for that task
+            //function will go through bids and best bid(robot) is selected
+        }
 
     }
     public static int[] generateInitialEnvironment(){//generates random positions
@@ -64,6 +109,7 @@ public class Main {
             Robot robot = new Robot();
             robot.setRow(row);
             robot.setCol(column);
+            robot.setID(i);
             robots.add(robot);
         }
         for(int i = 5; i < positions.length; i++) //fills the garbage/rubble positions
