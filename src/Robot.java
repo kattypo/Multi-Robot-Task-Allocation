ed8852b5b8c0ff5d424936f80fbb4530c09ec5d9
kattypo <74074509+kattypo@users.noreply.github.com>
2023-04-30 23:14:11 -0400

    Update Robot.java
    Added id for robot identification and methods to support it
    Changed boolean task to hasTask
    Int task now stores task id

--- src/Robot.java
+++ src/Robot.java
@@ -1,29 +1,39 @@
 import java.lang.Math;
 
 public class Robot {
-    int bid = 0; //stores current bid for task
-    boolean task = false; //if robot is currently assigned task
+    int bid = -1; //stores current bid for task
+    boolean hasTask = false; //if robot is currently assigned task
     int row;
     int col;
+    int id; //to keep track of which robot is which
+    int task;
 
     int calculateBid(int row, int col, int goalRow, int goalCol){ //takes in task location & returns bid for task
-        bid = (int) Math.round(Math.sqrt( (goalCol - col)^2 - (goalRow - row)^2));
+        bid = (int) Math.round(Math.sqrt(Math.pow((goalCol - col), 2) + Math.pow((goalRow - row), 2)));
         return bid;
     }
 
-    void assignTask(){ //robot has task
+    void assignTask(int taskId){ //robot has task
 
-        task = true;
+        hasTask = true;
+        task = taskId;
     }
     void unassignTask(){ //robot has no task
-        if(task){
-            task = false;
+        if(hasTask){
+            hasTask = false;
         }
     }
     void setRow(int y){
+
         row = y;
     }
     void setCol(int x){
+
         col = x;
     }
+    void setID(int ID){
+
+        id = ID;
+    }
 }
+
