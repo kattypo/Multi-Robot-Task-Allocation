b0d275bbe3997b74bda30d603687f2d202d7b5b6
kattypo <74074509+kattypo@users.noreply.github.com>
2023-05-09 14:12:10 -0400

    Create Obstacle.java
    A class for the creation of obstacles

---
+++ src/Obstacle.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Obstacle {
+    int row;
+    int col;
+
+    void setRow(int y){
+
+        row = y;
+    }
+    void setCol(int x){
+
+        col = x;
+    }
+}
+
