05cb254822467763a6ccc3929415a8d96725c666
kattypo <kturne9890@gmail.com>
2024-09-19 02:31:50 -0400

    Update Visualizer2.java
    Removed button for code advancement and replaced with a 1 second timer

--- src/Visualizer2.java
+++ src/Visualizer2.java
@@ -1,42 +1,34 @@
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.*;
 import javax.swing.*;
 import java.awt.*;
+import java.util.Timer;
 
 public abstract class Visualizer2 implements ActionListener {
     static ArrayList<Robot> robots = new ArrayList<>();
     static ArrayList<Task> tasks = new ArrayList<>();
-
     static ArrayList<Obstacle> obstacles = new ArrayList<>();
-
-    static int cyclesCounter = 1;//starts at one so the program waits for user to press button before updating
     static JFrame grid = new JFrame();
     static JPanel bottom = new JPanel();//displays grid
     static JPanel top = new JPanel();//displays button
     static int idAssigner = 0; //gives each task a unique id
-    static char[][] initialGrid;
 
     public static void demo(){
-
-        int cycles = 1; //amount of cycles to demo for
-        AStarPathGenerator path = new AStarPathGenerator();//added
-        JButton button = new JButton("Progress 1 step");
+        AStarPathGenerator path = new AStarPathGenerator();
         int[] initialPositions = generateInitialEnvironment();
-        initialGrid = generateGrid(initialPositions);
-        displayGrid(initialGrid, grid, button);
+        char[][] initialGrid = generateGrid(initialPositions);
+        displayGrid(initialGrid, grid);
+
         for(int j = 0; j < tasks.size(); j++) {
             tasks.get(j).setId(idAssigner);
             idAssigner += 1;
         }
-
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {//responds to button press
-                resetCounter();//sets counter to zero so it updates
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            public void run(){
                 boolean duplicate = false;
-                while(cyclesCounter < cycles){ //will iterate for determined amount of cycles
                     for(int i = 0; i < robots.size(); i++){//robots should view list of tasks and pick one
-                        if((!robots.get(i).hasTask) && (tasks.size() > 0)){//verifies robot is not assigned task and there are tasks to assign
+                        if((!robots.get(i).hasTask) && (tasks.size() > 0)) { //verifies robot is not assigned task and there are tasks to assign
                             for(int j = 0; j < tasks.size(); j++){
                                 if(tasks.get(j).assigned() < 2){
                                     int robotBid = robots.get(i).calculateBid(robots.get(i).row, robots.get(i).col, tasks.get(j).row, tasks.get(j).col);
@@ -46,11 +38,14 @@ public abstract class Visualizer2 implements ActionListener {
                             }
                         }
                     }
-
-                    for(int i = 0; i < tasks.size(); i++){//sorts the bids in each tasks
-                        tasks.get(i).sortBids();
+                    if(tasks.size() == 0) {
+                        timer.cancel();
+                        return;
                     }
 
+                    for(int i = 0; i < tasks.size(); i++) {//sorts the bids in each tasks
+                        tasks.get(i).sortBids();
+                    }
                     Collections.sort(tasks);//sorts the tasks by best preferred match for assignment
 
                     for(int place = 0; place < robots.size(); place++){ //will go through each task's preferred robot 1st,2nd, etc.
@@ -107,52 +102,47 @@ public abstract class Visualizer2 implements ActionListener {
                             }
                         }
                     }
-                    cyclesCounter += 1;
-                }
-                initialGrid = updateGrid();
+                updateGrid();
             }
-        });
+        }, 0, 1000);
     }
     public static int[] generateInitialEnvironment(){//generates random positions
-        //don't do number repeats and positions should be between 0-2500
-        //return positions of those members for calculations later
-        //the first 20 numbers are the robots, the next 40 are garbage, and the rest are obstacles
-        int robots = 20;
-        int garbage = 40;
-        int obstacles = 20;
-        int[] positions = new int[robots + garbage + obstacles];
-        int testValue;
+        //positions should be between 0-2499 inclusive
+        //first 20 positions are robots, next 40 are garbage, and the rest are obstacles
+
+        final int robots = 20;
+        final int garbage = 40;
+        final int obstacles = 20;
+        final int maxValue = 2499;
+        final int totalPositions = robots + garbage + obstacles;
+        Set<Integer> uniquePositions = new HashSet<>();
         Random random = new Random();
-        for(int i = 0; i < positions.length; i++) { //generates random positions
-            boolean duplicate = false;
-            do {
-                testValue = random.nextInt(((2500) - 1));
-                duplicate = false;
-                for (int j = 0; j < i; j++) {
-                    if (positions[j] == testValue) {
-                        duplicate = true;
-                        break;
-                    }
-                }
-            }
-            while (duplicate);
-            positions[i] = testValue;
+
+        while(uniquePositions.size() < totalPositions) {
+            int testValue = random.nextInt(maxValue);
+            uniquePositions.add(testValue);
+        }
+        int iterator = 0;
+        int[] positions = new int[totalPositions];
+        
+        for(Integer position: uniquePositions) {
+            positions[iterator] = position.intValue();
+            iterator += 1;
         }
         return positions;
     }
-    public static char[][] generateGrid(int[] positions) //creates grid
-    {
+    public static char[][] generateGrid(int[] positions) { //creates grid
         //first 20 positions are robots, 40 are garbage, and the rest are obstacles
+
         char[][] result = new char[50][50]; //creates environment for display
-        for(int i = 0; i < 50; i++)
-        {
+
+        for(int i = 0; i < 50; i++) {
             Arrays.fill(result[i], '-');
         }
-        for(int i = 0; i < 20; i++) //fills the robots positions
-        {
+
+        for(int i = 0; i < 20; i++) { //fills the robots positions
             int row = positions[i] / 50;
             int column = positions[i] % 50;
-
             result[row][column] = 'R';
             Robot robot = new Robot();
             robot.setRow(row);
@@ -160,22 +150,19 @@ public abstract class Visualizer2 implements ActionListener {
             robot.setID(i);
             robots.add(robot);
         }
-        for(int i = 20; i < 60; i++) //fills the garbage positions
-        {
+        for(int i = 20; i < 60; i++) { //fills the garbage positions
             int row = positions[i] / 50;
             int column = positions[i] % 50;
-
             result[row][column] = 'G';
             Task task = new Task();
             task.setRow(row);
             task.setCol(column);
             tasks.add(task);
         }
-        for(int i = 60; i < positions.length; i++) //fills the obstacle positions
-        {
+
+        for(int i = 60; i < positions.length; i++) { //fills the obstacle positions
             int row = positions[i] / 50;
             int column = positions[i] % 50;
-
             result[row][column] = '.';
             Obstacle obstacle = new Obstacle();
             obstacle.setRow(row);
@@ -185,11 +172,10 @@ public abstract class Visualizer2 implements ActionListener {
         return result;
     }
 
-    public static void displayGrid(char[][] initialGrid, JFrame grid, JButton button){
+    public static void displayGrid(char[][] initialGrid, JFrame grid){
         top.setBackground(Color.gray);
         bottom.setLayout(new GridLayout(50,50));
         bottom.setSize(800,800);
-        top.add(button);
 
         JLabel[][] cells;
         cells = new JLabel[50][50];
@@ -222,28 +208,28 @@ public abstract class Visualizer2 implements ActionListener {
         grid.setVisible(true);
     }
 
-    public static char[][] updateGrid(){
+    public static void updateGrid(){
         JLabel[][] cells;
         cells = new JLabel[50][50];
         char[][] result = new char[50][50]; //creates environment for display
         bottom.removeAll();
-        for(int i = 0; i < 50; i++)
-        {
+
+        for(int i = 0; i < 50; i++) {
             Arrays.fill(result[i], '-');
         }
 
-        for(int i = 0; i < tasks.size(); i++){
+        for(int i = 0; i < tasks.size(); i++) {
             result[tasks.get(i).row][tasks.get(i).col] = 'G';
-
         }
-        for(int j = 0; j < robots.size(); j++){
+
+        for(int j = 0; j < robots.size(); j++) {
             result[robots.get(j).row][robots.get(j).col] = 'R';
-
         }
-        for(int i = 0; i < obstacles.size(); i++){
+
+        for(int i = 0; i < obstacles.size(); i++) {
             result[obstacles.get(i).row][obstacles.get(i).col] = '.';
-
         }
+
         for (int i = 0; i < 50; i++) {
             for (int j = 0; j < cells.length; j++) {
                 cells[i][j] = new JLabel(" " + result[i][j] + " ", SwingConstants.CENTER);
@@ -261,13 +247,8 @@ public abstract class Visualizer2 implements ActionListener {
                 bottom.add(cells[i][j]);
             }
         }
-
         bottom.revalidate();
         bottom.repaint();
-        return result;
-    }
-    static void resetCounter(){//for use with button, selection will reset the counter
-        cyclesCounter = 0;
     }
 
     static int findTask(int value){
@@ -283,6 +264,5 @@ public abstract class Visualizer2 implements ActionListener {
             System.out.println("Error: id not found");
         }
         return goalTask;
-
     }
 }
