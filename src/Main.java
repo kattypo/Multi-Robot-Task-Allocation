631e2a8cf9a404b103716aae7f6893b7b40e949b
kattypo <kturne9890@gmail.com>
2024-09-18 22:21:17 -0400

    Update Main.java
    Improved readability and added exception handling

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,35 @@
 import java.util.*;
 public abstract class Main  {
-    //design specs:
-    // robots - full commitment (finish task once started)
-    // coordinated strategy (two robots on one task max)
-    // option 1 has no obstacles
-    // option 2 has obstacles
+    //Design specs:
+    // Robots - full commitment (finish task once started)
+    // Coordinated strategy (two robots on one task max)
+    // Option 1 has no obstacles
+    // Option 2 has obstacles
 
     public static void main(String[] args) {
 
-        System.out.print("Choose a option\n Option 1: demo with no obstacles\n Option 2: demo with obstacles\n");
         Scanner scanner = new Scanner(System.in);
-        int option = scanner.nextInt();
-        if (option == 1) {
-            Visualizer1.demo();
-        }
-        else if (option == 2) {
-            Visualizer2.demo();
-        }
-        else {
+        System.out.print("Choose a option\n Option 1: Demo with no obstacles\n Option 2: Demo with obstacles\n");
+        
+        try {
+            int option = scanner.nextInt();
+
+            switch(option) {
+                case 1:
+                    Visualizer1.demo();
+                    break;
+                case 2:
+                    Visualizer2.demo();
+                    break;
+                default:
+                    System.out.println("Invalid input. Try again.");
+            }
+        } 
+        catch(Exception e) {
             System.out.println("Invalid input. Try again.");
+        } 
+        finally {
+            scanner.close();
         }
     }
 }
